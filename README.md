9adff4819f6994e00e062d32394bbf608406ac26
unknown <36110557+bibek63@users.noreply.github.com>
2022-06-23 08:00:06 +0545

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+TIKTOK CLONE
+
+Tiktok clone made with flutter and firebase.
+Getx is used as state management.
+
+Auth is handled using firebase authentication system
+
+User can post and view videos.
+
+Social media functionality such as likes, comments, and follow is implemented.
+
+![Login](images/login.jpg)
+
+![Register](images/register.jpg)
+
+![Home](images/home.jpg)
+
+![Profile](images/profile.jpg)
---
+++ lib/views/screens/auth/signup_screen.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/controllers/auth_controller.dart';
+import 'package:tiktok_clone/views/screens/auth/login_screen.dart';
+
+import '../../../constanst.dart';
+import '../../widgets/text_input_field.dart';
+
+class SignupScreen extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Tiktok Clone",
+                    style: TextStyle(
+                      fontSize: 35,
+                      color: buttonColor,
+                      fontWeight: FontWeight.w900,
+                    )),
+                const Text("Register",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700,
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+                Stack(
+                  children: [
+                    const CircleAvatar(
+                      radius: 64,
+                      backgroundImage: NetworkImage(
+                          'https://media.comicbook.com/2021/07/dragon-ball-super-new-super-hero-movie-goku-toei-animation-1276890.jpeg?auto=webp'),
+                      backgroundColor: Colors.black,
+                    ),
+                    Positioned(
+                        bottom: -10,
+                        left: 80,
+                        child: IconButton(
+                          onPressed: () => authController.pickImage(),
+                          icon: const Icon(Icons.add_a_photo),
+                        ))
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextInputField(
+                    controller: _usernameController,
+                    labelText: "Username",
+                    icon: Icons.person,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextInputField(
+                    controller: _emailController,
+                    labelText: "Email",
+                    icon: Icons.email,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextInputField(
+                    controller: _passwordController,
+                    labelText: "Password",
+                    isObsecure: true,
+                    icon: Icons.lock,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 40,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: buttonColor,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: InkWell(
+                    onTap: () => authController.registerUser(
+                        _usernameController.text,
+                        _emailController.text,
+                        _passwordController.text,
+                        authController.profilePhoto),
+                    child: const Center(
+                      child: Text(
+                        "Register",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account?",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => LoginScreen()));
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 20, color: buttonColor),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
